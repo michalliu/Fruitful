ad3e66f2b3c97eea95e0c7c039033c5331140a03
michalliu <michalliu@foxmail.com>
2015-08-30 17:49:33 +0800

    显示标题

--- content-page.php
+++ content-page.php
@@ -12,10 +12,10 @@
 	<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
 		<div class="entry-thumbnail"><?php the_post_thumbnail(); ?></div>
 	<?php endif; ?>
-	<?php if (!is_front_page()) {?>	
-	<!-- <header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-	</header> --><!-- .entry-header -->
+	<?php if (!is_front_page()) {?>
+	<header class="entry-header">
+		<p class="entry-title"><?php the_title(); ?></p>
+	</header>
 	<?php } ?>
 	<div class="entry-content">
 		<?php the_content(); ?>
