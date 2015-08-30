2e0b23eeb5b8993bddd8a60cac980cc0f2e2689b
michalliu <michalliu@foxmail.com>
2015-08-31 04:03:34 +0800

    首页不显示分享按钮

--- content-page.php
+++ content-page.php
@@ -7,26 +7,29 @@
  * @since Fruitful theme 1.0
  */
 ?>
+<?php $is_doc_frontpage=preg_replace('/^https?:\/\//','',get_the_permalink()) == $_SERVER['SERVER_NAME']."/doc/"; ?>
+<?php if(!is_front_page() &&
+    !$is_doc_frontpage) : ?>
 <div class="share-buttons">
     <?php fruitful_the_sharebuttons(); ?>
 </div>
+<?php endif; ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <div class="post-content">
 	<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
 		<div class="entry-thumbnail"><?php the_post_thumbnail(); ?></div>
 	<?php endif; ?>
-    <?php $doc_frontpage=preg_replace('/^https?:\/\//','',get_the_permalink());?>
     <?php if (!is_front_page() && // doc首页不显示标题，其它页面显示
-        $doc_frontpage != $_SERVER['SERVER_NAME']."/doc/") {?>
+        !$is_doc_frontpage) {?>
         <header class="post-header">
-            <h1 class="post-title entry-title"><?php the_title(); ?></h1>
+            <h1 class="post-title entry-title"><?php echo ucfirst(get_the_title()); ?></h1>
         </header>
+        <div class="meta-section tags">
+                <span class="meta-value">
+                    <span class="tutorial-date"><?php echo get_the_date(); ?></span>
+                </span>
+        </div>
     <?php } ?>
-    <div class="meta-section tags">
-            <span class="meta-value">
-                <span class="tutorial-date"><?php echo get_the_date(); ?></span>
-            </span>
-    </div>
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'fruitful' ), 'after' => '</div>' ) ); ?>
